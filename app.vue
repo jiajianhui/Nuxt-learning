c1b26a8a32a6f265a5c1ab42af7c0f02588679fa
jianhui <78072865+jiajianhui@users.noreply.github.com>
2024-12-15 11:40:12 +0800

    升级vite到最新版本，安装vercel的网页统计插件

--- app.vue
+++ app.vue
@@ -1,8 +1,13 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <!-- <NuxtWelcome /> -->
     <NuxtLayout>
+      <Analytics />
       <NuxtPage />
     </NuxtLayout>
   </div>
