626337ad84080dbf34090e1607be723056c2057b
jianhui <78072865+jiajianhui@users.noreply.github.com>
2024-12-18 20:52:41 +0800

    更改网站标题和Favicon

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
 import { Analytics } from '@vercel/analytics/nuxt';
+
+//SEO
+useHead({
+  // 动态标题
+  titleTemplate: '%s - jianhui.fun',
+  // 打印
+  script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
 </script>
+
 
 <template>
   <div>
